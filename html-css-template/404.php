ed0ea4d7b9d3cb5801aa4f6c7ea88b3bf1c9710c
Thomas Maxwell <hello@tom-maxwell.com>
2014-11-08 17:26:25 +0000

    basic error page template

---
+++ html-css-template/404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php  	include ('head.php'); ?>
+</head>
+<body>
+	<header>
+		<?php include('header.php'); ?>
+	</header>
+	<article id="error">
+		<h1 id="code">404!</h1>
+		<h1>Opps it seems this page doesn't exist, sorry about that!</h1>
+		<h2>Possible reasons include:</h2>
+		<ul>
+			<li>A black hole sucked it up</li>
+			<li>The aliens invaded, again</li>
+			<li>We made a mistake</li>
+			<li>The URL was typed wrongly</li>
+		</ul>
+	</article>
+	<footer>
+		<?php include('footer.php'); ?>
+	</footer>
+</body>
+</html>

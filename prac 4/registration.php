ff4916165a1e28ebc4a968fafaee0cde870b5465
Aidan O'Grady <theaidanogrady@gmail.com>
2014-11-13 15:03:12 +0000

    Almost completed, changing theme on DEVWEB needs fixed

--- prac 4/registration.php
+++ prac 4/registration.php
@@ -10,18 +10,15 @@
 
 		// Connect to database server
 		mysql_connect("devweb2014.cis.strath.ac.uk", "wlb12153", "ingeduel");
-		echo "Connected to MySQL<br/>";
 
 		// Select your database
 		mysql_select_db("wlb12153") or die(mysql_error());
-		echo "Connected to Database<br/>";
 
 		// Build an SQL query to add the new entry into the registration table
 		$QUERY = "INSERT INTO registration (email, surname, forename) 
 		VALUES ('$email', '$surname', '$forename')";
 		// Execute the query
 		mysql_query($QUERY) or die(mysql_error());
-		echo "Entry added.";
 		// Redirect to index.php
 		header("Location: https://devweb2014.cis.strath.ac.uk/~wlb12153/WAD/practicals/prac4/registered.php");
 	
@@ -32,7 +29,6 @@
 		// Print out an error message "Form not filled in correctly..." and
 		// provide a link back to addEntry.html
 		echo "Form not filled in correctly.<br/>";
-		echo '<a href=https://devweb2014.cis.strath.ac.uk/~wlb12153/WAD/practicals/prac2/Registration.html>Try again.</a>';
 
 	}
 ?>

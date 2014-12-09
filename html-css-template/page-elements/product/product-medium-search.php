385aa772f67880a250c47273f4ec70bf4448567b
Aidan O'Grady <theaidanogrady@gmail.com>
2014-12-09 15:03:02 +0000

    Changed search layout, needs SASS though

--- html-css-template/page-elements/product/product-medium-search.php
+++ html-css-template/page-elements/product/product-medium-search.php
@@ -1,31 +1,13 @@
 <div class="product-medium-search product-medium">
 	<a href="product-page.php">
-	<img src='<?php echo $image ?>' alt="Product image">
-	<div class="product-info">
-		<h4><?php echo $name ?></h4>
-		<p><?php echo $price ?></p>
-	</div>
+		<div class="img">
+			<img src="<?php echo $image; ?>" alt="Product image">
+		</div>
+		<div class="product-info">
+			<h4><?php echo $name;?></h4>
+			<p><?php echo "£".$price;?></p>
+			<p><?php echo $category;?></p>
+			<p><?php echo $description;?></p>
+		</div>
 	</a>
-	<div class="picked-options">
-		<ul id="adult-list">
-			<li>
-				<ul id="child-list">
-					<li>Colour:</li>
-					<li id="colour"><?php echo $colour ?></li>
-				</ul>
-			</li>
-			<li>
-				<ul id="child-list">
-					<li>Size:</li>
-					<li id="size"><?php echo $size ?></li>
-				</ul>
-			</li>
-			<li>
-				<ul id="child-list">
-					<li>Quantity:</li>
-					<li id="quantity"><?php echo $quantity?></li>
-				</ul>
-			</li>
-		</ul>
-	</div>
 </div>

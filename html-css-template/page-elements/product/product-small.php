6ed6086a80f26a9d369c03f392060a6e173f0b49
Aidan O'Grady <theaidanogrady@gmail.com>
2014-12-10 13:20:12 +0000

    Products under 'You might also love to wear' now link correctly

--- html-css-template/page-elements/product/product-small.php
+++ html-css-template/page-elements/product/product-small.php
@@ -1,5 +1,5 @@
 <div class="product-small">
-	<a href="#">
+	<a href=<?php echo "product-page.php?item=".str_replace(" ", "+", $name);?>>
 	<img src="<?php echo $image; ?>" alt="Product Image">
 	<div class="product-info">
 		<z><?php echo $name ?></h4>

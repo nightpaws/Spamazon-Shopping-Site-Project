5371858b1ec7b725304940946df9d1a0ec55e7cb
Aidan O'Grady <theaidanogrady@gmail.com>
2014-12-10 21:17:22 +0000

    Fixed error in term term was undefined variable

--- html-css-template/databaseFunct/search.php
+++ html-css-template/databaseFunct/search.php
@@ -1,6 +1,7 @@
 <?php
     include('databaseconnection.php');
 
+    $term = $_GET["term"];
     //execute the SQL query and return records
     $result = mysql_query("SELECT * FROM cs312_stock WHERE name LIKE '%$term%'
         OR category LIKE '%$term%' OR description LIKE '%$term%'");

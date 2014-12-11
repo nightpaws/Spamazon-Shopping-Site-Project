83b39b9c4cb3ac3c8943375a34eb08ff48f3272e
nightpaws <nightpaws@hotmail.co.uk>
2014-12-11 00:38:33 +0000

    Added php for showing email
    Now shows current email of logged in user in top right.

--- html-css-template/page-elements/header-logged-in.php
+++ html-css-template/page-elements/header-logged-in.php
@@ -1,8 +1,12 @@
+<?php
+$cookie_name = "spamaznauth";
+$email = urldecode($_COOKIE[$cookie_name]);
+ ?>
 <a href="#user-menu" title="Show navigation">Show navigation<span id="nav-icon user-icon"></a>
 <a href="#" title="Hide navigation">Hide navigation<span class="nav-icon user-icon"></a>
 <ul>
 	<li>
-		<a href="account-man.php">EMAIL HERE</a>
+		<a href="account-man.php"><?php echo $email ?></a>
 		<ul>
 			<li><a href='index.php?logout=true'>Log Out</a></li>
 		</ul>

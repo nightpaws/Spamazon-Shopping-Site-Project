e2b9fb6511775920d29369596cd1a60e167c0c01
Thomas Maxwell <hello@tom-maxwell.com>
2014-12-12 18:08:46 +0000

    Readded flexslider after aidan fucked up and deleted all of the universe

--- html-css-template/page-elements/head.php
+++ html-css-template/page-elements/head.php
@@ -6,7 +6,6 @@
 <link rel="stylesheet" href="css/reset.css">
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<!-- <script type="text/javascript" src="./js/menu.js"></script> -->
 <!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
 <?php /* Loading in the stuff for the slider if its needed on the page */ ?>
